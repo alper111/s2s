f0a44bc2ea06e4129598de6ab307bb407d05d9ae
alper111 <ahmetoglu.alper@gmail.com>
2024-04-09 15:06:21 -0400

    more strict typing

--- estimators.py
+++ estimators.py
@@ -1,9 +1,9 @@
-from typing import List, Iterable
-
 import numpy as np
 from sklearn.model_selection import GridSearchCV
 from sklearn.svm import SVC
 from sklearn.neighbors import KernelDensity
+
+from data import S2SDataset
 
 
 __author__ = 'Steve James and George Konidaris'
@@ -17,23 +17,23 @@ class SupportVectorClassifier:
     uses support vector machines with Platt scaling.
     """
 
-    def __init__(self, mask: List[int], probabilistic=True):
+    def __init__(self, mask: list[int], probabilistic=True):
         """
         Create a new SVM classifier for preconditions
 
         Parameters
         ----------
-        mask : List[int]
+        mask : list[int]
             The state variables that should be kept for classification
         probabilistic : bool, optional
             Whether the classifier is probabilistic
         """
         self._mask = mask
         self._probabilistic = probabilistic
-        self._classifier: SVC = None
+        self._classifier: SVC | None = None
 
     @property
-    def mask(self) -> List[int]:
+    def mask(self) -> list[int]:
         """
         Get the precondition mask
         """
@@ -58,7 +58,7 @@ class SupportVectorClassifier:
         c_range = kwargs.get('precondition_c_range', np.arange(1, 16, 2))
         gamma_range = kwargs.get('precondition_gamma_range', np.arange(4, 22, 2))
 
-        param_grid = dict(gamma=gamma_range, C=c_range)
+        param_grid = {'gamma': gamma_range, 'C': c_range}
         grid = GridSearchCV(SVC(class_weight='balanced'), param_grid=param_grid, cv=3, n_jobs=-1)  # 3 fold CV
         data = X[:, self.mask]
         grid.fit(data, y)
@@ -88,6 +88,7 @@ class SupportVectorClassifier:
         float
             The probability of the state according to the classifier.
         """
+        assert isinstance(self._classifier, SVC), "Classifier not trained yet"
         if states.ndim == 1:
             states = states.reshape(1, -1)
 
@@ -103,17 +104,17 @@ class KernelDensityEstimator:
     A density estimator that models a distribution over low-level states.
     """
 
-    def __init__(self, mask: List[int]):
+    def __init__(self, mask: list[int]):
         """
         Initialize a new estimator.
 
         Parameters
         ----------
-        mask : List[int]
+        mask : list[int]
             The state variables we care about.
         """
         self._mask = mask
-        self._kde: KernelDensity = None
+        self._kde: KernelDensity | None = None
 
     def fit(self, X: np.ndarray, **kwargs) -> None:
         """
@@ -142,7 +143,7 @@ class KernelDensityEstimator:
         self._kde = grid.best_estimator_
 
     @property
-    def mask(self) -> List[int]:
+    def mask(self) -> list[int]:
         """
         Get the effect mask.
         """
@@ -162,18 +163,19 @@ class KernelDensityEstimator:
         data : np.ndarray
             An array of size [n_samples, len(mask)] containing the sampled data.
         """
+        assert isinstance(self._kde, KernelDensity), "Estimator not trained yet"
         data = self._kde.sample(n_samples)
         if data.ndim == 1:  # ensure always shape of (N X D)
             return np.reshape(data, (data.shape[0], 1))
         return data
 
-    def integrate_out(self, variable_list: Iterable[int], **kwargs) -> 'KernelDensityEstimator':
+    def integrate_out(self, variable_list: list[int], **kwargs) -> 'KernelDensityEstimator':
         """
         Integrate out the given variables from the distribution.
 
         Parameters:
         ----------
-        variable_list : Iterable[int]
+        variable_list : list[int]
             A list of variables to be marginalized out from the distribution.
 
         Returns:
@@ -183,8 +185,8 @@ class KernelDensityEstimator:
         """
         variable_list = sorted(variable_list)  # make sure it's always sorted to prevent bugs!
 
-        new_vars = list()
-        new_indices = list()
+        new_vars = []
+        new_indices = []
 
         # find all the other variables in the mask except what's given
         for pos, val in enumerate(self.mask):  # TODO probably a better way of doing this in numpy
@@ -197,3 +199,7 @@ class KernelDensityEstimator:
         kwargs['masked'] = True  # the data has already been masked
         kde.fit(new_samples, **kwargs)
         return kde
+
+
+def learn_preconditions(subgoals: dict[tuple[int, int], S2SDataset]) -> dict[tuple[int, int], SupportVectorClassifier]:
+    raise NotImplementedError
--- partition.py
+++ partition.py
@@ -56,7 +56,7 @@ def partition_to_subgoal(dataset: S2SDataset) -> dict[tuple[int, int], S2SDatase
     return partitions
 
 
-def _split_by_options(dataset: S2SDataset) -> list[S2SDataset]:
+def _split_by_options(dataset: S2SDataset) -> dict[int, S2SDataset]:
     """
     Split a dataset by options.
 
@@ -126,7 +126,7 @@ def _partition(x: np.ndarray) -> dict[int, list]:
     return partitions
 
 
-def _k_means(x: np.ndarray, k: int, centroids: np.ndarray = None) -> tuple[np.ndarray, np.ndarray, float]:
+def _k_means(x: np.ndarray, k: int, centroids: np.ndarray | None = None) -> tuple[np.ndarray, np.ndarray, float]:
     """
     Perform k-means clustering.
 
@@ -256,8 +256,10 @@ def _x_means(x: np.ndarray, k_min: int, k_max: int) -> tuple[np.ndarray, np.ndar
         The mean squared error.
     """
     k = k_min
-    centroids = None
+    assert k_min < k_max and k_min > 0
     while k < (k_max+1):
+        if k == k_min:
+            centroids = None
         centroids, assigns, _ = _k_means(x, k, centroids=centroids)
         centroid_list = []
         change = False
