2e214b1aadbc60218e434a7b5fc6c9dd16afb15c
alper111 <ahmetoglu.alper@gmail.com>
2024-04-09 11:05:32 -0400

    generic data collection script

---
+++ collect.py
@@ -0,0 +1,42 @@
+from typing import Callable
+
+import gymnasium as gym
+import numpy as np
+
+from data import S2SDataset
+
+
+def collect(n: int, env: gym.Env, options: dict[str, Callable]) -> S2SDataset:
+    state_arr = np.zeros((n,) + env.observation_space.shape, dtype=np.float32)
+    action_arr = np.zeros(n, dtype=np.int64)
+    reward_arr = np.zeros(n, dtype=np.float32)
+    next_state_arr = np.zeros((n,) + env.observation_space.shape, dtype=np.float32)
+    mask_arr = np.zeros((n,) + env.observation_space.shape, dtype=bool)
+
+    i = 0
+    while i < n:
+        state, _ = env.reset()
+        done = False
+
+        while not done and i < n:
+            if options is None:
+                action = env.action_space.sample()
+            else:
+                # TODO:
+                # select a random option o with I_o > 0
+                # execute it in a loop till it terminates
+                pass
+            next_state, reward, term, trun, _ = env.step(action)
+            opt_mask = next_state != state
+
+            state_arr[i] = state
+            action_arr[i] = action
+            reward_arr[i] = reward
+            next_state_arr[i] = next_state
+            mask_arr[i] = opt_mask
+
+            done = term or trun
+            state = next_state
+            i += 1
+
+    return S2SDataset(state_arr, action_arr, reward_arr, next_state_arr, mask_arr)
