a3bc286203600a99f4ba173b00bcdebe95e602db
alper111 <ahmetoglu.alper@gmail.com>
2024-05-02 19:04:15 -0400

    obj-centric demo

--- demo.py
+++ demo.py
@@ -1,9 +1,6 @@
 import pickle
 
-from gymnasium.wrappers.flatten_observation import FlattenObservation
-from gymnasium.wrappers.transform_observation import TransformObservation
-
-from environment import MNIST8Tile
+from environment import MNIST8Tile, ObjectCentricEnv
 from collect import collect
 from partition import partition_to_subgoal
 from learn_ops import learn_operators
@@ -11,18 +8,19 @@ from build_vocab import build_vocabulary, build_schemata
 
 
 # initialize the environment
-env = FlattenObservation(MNIST8Tile(random=True, max_steps=1))
-env = TransformObservation(env, lambda x: x / 255)
+env = ObjectCentricEnv(MNIST8Tile(random=False, max_steps=1))
 
 # collect data
 print("Collecting data...")
-dataset = collect(50000, env, None)
+dataset = collect(10000, env, None)
 print("Data collected.")
 
 # partition the data s.t. subgoal property is satisfied
 print("Partitioning data...")
 partitions = partition_to_subgoal(dataset)
 print(f"Number of partitions={len(partitions)}.")
+with open("partitions.pkl", "wb") as f:
+    pickle.dump(partitions, f)
 
 # learn precondition classifiers and effect predictors
 print("Learning operators...")
@@ -33,14 +31,11 @@ print(f"Number of operators={len(ops)}.")
 # ops = pickle.load(open("ops.pkl", "rb"))
 
 print("Building vocabulary...")
-factors, vocabulary, propositions = build_vocabulary(ops, 84*84)
-with open("factors.pkl", "wb") as f:
-    pickle.dump(factors, f)
+vocabulary, propositions = build_vocabulary(ops, 786)
 with open("vocabulary.pkl", "wb") as f:
     pickle.dump(vocabulary, f)
 with open("propositions.pkl", "wb") as f:
     pickle.dump(propositions, f)
-print(f"Number of factors={len(factors)}.")
 print(f"Vocabulary size={len(vocabulary)}.")
 print(f"Number of propositions={len(propositions)}.")
 # factors = pickle.load(open("factors.pkl", "rb"))
@@ -49,7 +44,7 @@ print(f"Number of propositions={len(propositions)}.")
 
 
 print("Building schemata...")
-schemata = build_schemata(factors, ops, vocabulary, propositions)
+schemata = build_schemata(ops, vocabulary, propositions)
 with open("schemata.pkl", "wb") as f:
     pickle.dump(schemata, f)
 for schema in schemata:
