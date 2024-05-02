e528cfac6dbd8f1e57680e9906ed1a657b79ea16
alper111 <ahmetoglu.alper@gmail.com>
2024-05-02 10:38:06 -0400

    add oo-centric data collection

--- collect.py
+++ collect.py
@@ -4,20 +4,24 @@ import gymnasium as gym
 import numpy as np
 
 from structs import S2SDataset
-from environment import S2SEnv, ObjectCentricEnv
+from environment import ObjectCentricEnv
 
 
-def collect(n: int, env: S2SEnv | ObjectCentricEnv, options: dict[str, Callable] | None = None) -> S2SDataset:
+def collect(n: int, env: gym.Env | ObjectCentricEnv, options: dict[str, Callable] | None = None) -> S2SDataset:
     if isinstance(env.observation_space, gym.spaces.Box):
-        shape = (n,) + env.observation_space.shape
+        obs_shape = (n,) + env.observation_space.shape
+        act_shape = (n,)
     elif isinstance(env.observation_space, gym.spaces.Sequence):
         assert isinstance(env.observation_space.feature_space, gym.spaces.Box)
-        shape = (n, env.max_objects) + env.observation_space.feature_space.shape
-    state_arr = np.zeros(shape, dtype=np.float32)
-    action_arr = np.zeros(n, dtype=np.int64)
+        assert isinstance(env, ObjectCentricEnv)
+        obs_shape = (n, env.max_objects) + env.observation_space.feature_space.shape
+        act_shape = (n, 2)
+
+    state_arr = np.zeros(obs_shape, dtype=np.float32)
+    action_arr = np.zeros(act_shape, dtype=int)
     reward_arr = np.zeros(n, dtype=np.float32)
-    next_state_arr = np.zeros(shape, dtype=np.float32)
-    mask_arr = np.zeros(shape, dtype=bool)
+    next_state_arr = np.zeros(obs_shape, dtype=np.float32)
+    mask_arr = np.zeros(obs_shape, dtype=bool)
 
     i = 0
     while i < n:
@@ -32,8 +36,12 @@ def collect(n: int, env: S2SEnv | ObjectCentricEnv, options: dict[str, Callable]
                 # select a random option o with I_o > 0
                 # execute it in a loop till it terminates
                 pass
-            next_state, reward, term, trun, _ = env.step(action)
-            opt_mask = env.get_mask(state, next_state)
+            if isinstance(env, ObjectCentricEnv):
+                next_state, reward, term, trun, info = env.step(state, action)
+                action = np.array([action, info["acted_object"]])
+            else:
+                next_state, reward, term, trun, _ = env.step(action)
+            opt_mask = state != next_state
 
             state_arr[i] = state
             action_arr[i] = action
