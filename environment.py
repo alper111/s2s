124f3e3ad63af2d1d2953315416d0e6c736c14e4
alper111 <ahmetoglu.alper@gmail.com>
2024-06-14 16:47:36 -0400

    add some uncertainty to mnistsokoban

--- environment.py
+++ environment.py
@@ -6,17 +6,22 @@ from scipy.spatial.distance import cdist
 
 
 class MNISTSokoban(gym.Env):
-    metadata = {"render_modes": ["human", "rgb_array"], "render_fps": 100}
+    metadata = {"render_modes": ["human", "rgb_array"], "render_fps": 20}
 
     def __init__(self, map_file: str = None, size: tuple[int, int] = None, max_crates: int = 5, max_steps=200,
-                 render_mode: str = None):
+                 object_centric: bool = False, render_mode: str = None, rand_digits: bool = False,
+                 rand_agent: bool = False, rand_x: bool = False):
         assert map_file is not None or size is not None, "Either map_file or size must be provided"
 
         self._map_file = map_file
         self._size = size
         self._max_crates = max_crates
         self._max_steps = max_steps
+        self.object_centric = object_centric
         self.render_mode = render_mode
+        self.rand_digits = rand_digits
+        self.rand_agent = rand_agent
+        self.rand_x = rand_x
 
         self._shape = None
         self._window = None
@@ -27,16 +32,6 @@ class MNISTSokoban(gym.Env):
         self._delta = np.array([[0, 1], [-1, 0], [0, -1], [1, 0]])
         self._t = 0
 
-        # random points for drawing the cross
-        self._ax = np.random.randint(2, 6)
-        self._ay = np.random.randint(2, 6)
-        self._bx = np.random.randint(27, 31)
-        self._by = np.random.randint(27, 31)
-        self._cx = np.random.randint(27, 31)
-        self._cy = np.random.randint(2, 6)
-        self._dx = np.random.randint(2, 6)
-        self._dy = np.random.randint(27, 31)
-
         dataset = torchvision.datasets.MNIST(root="data", train=True, download=True)
 
         self._data = dataset.data.numpy()
@@ -45,7 +40,14 @@ class MNISTSokoban(gym.Env):
 
         self.action_space = gym.spaces.Discrete(4)
 
+        # for object-centric representation
+        self._feat_dim = 1024
+
     def reset(self) -> tuple[np.ndarray, dict]:
+        self._init_agent_mark()
+        self._init_x_mark()
+        self._init_digits()
+
         if self._map_file is not None:
             self._map = self.read_map(self._map_file)
         else:
@@ -53,9 +55,6 @@ class MNISTSokoban(gym.Env):
         self._shape = (len(self._map), len(self._map[0]))
         shape = (self._shape[0]*32, self._shape[1]*32)
         self.observation_space = gym.spaces.Box(low=0, high=255, shape=shape, dtype=np.uint8)
-        self._digit_idx = np.zeros(10, dtype=np.int64)
-        for i in self._labels:
-            self._digit_idx[i] = np.random.choice(self._labels[i])
 
         ax, ay = -1, -1
         for i in range(self._shape[0]):
@@ -80,6 +79,15 @@ class MNISTSokoban(gym.Env):
         pos = self._agent_loc
         next_pos = pos + self._delta[action]
         curr_bg, curr_tile = self._map[pos[0]][pos[1]]
+        if next_pos[0] < 0 or next_pos[0] >= self._shape[0] or next_pos[1] < 0 or next_pos[1] >= self._shape[1]:
+            self._t += 1
+            obs = self._get_obs()
+            info = self._get_info()
+            reward = self._get_reward()
+            terminated = reward > 1 - 1e-6
+            truncated = (self._t >= self._max_steps)
+            return obs, reward, terminated, truncated, info
+
         next_bg, next_tile = self._map[next_pos[0]][next_pos[1]]
 
         # the next tile is empty
@@ -94,12 +102,14 @@ class MNISTSokoban(gym.Env):
         else:
             # check whether the crate can be pushed
             further_pos = next_pos + self._delta[action]
-            further_bg, further_tile = self._map[further_pos[0]][further_pos[1]]
-            if further_tile == " ":
-                self._map[pos[0]][pos[1]] = (curr_bg, " ")
-                self._map[next_pos[0]][next_pos[1]] = (next_bg, "@")
-                self._map[further_pos[0]][further_pos[1]] = (further_bg, next_tile)
-                self._agent_loc = next_pos
+            if further_pos[0] >= 0 and further_pos[0] < self._shape[0] and \
+               further_pos[1] >= 0 and further_pos[1] < self._shape[1]:
+                further_bg, further_tile = self._map[further_pos[0]][further_pos[1]]
+                if further_tile == " ":
+                    self._map[pos[0]][pos[1]] = (curr_bg, " ")
+                    self._map[next_pos[0]][next_pos[1]] = (next_bg, "@")
+                    self._map[further_pos[0]][further_pos[1]] = (further_bg, next_tile)
+                    self._agent_loc = next_pos
 
         self._t += 1
 
@@ -109,30 +119,59 @@ class MNISTSokoban(gym.Env):
         terminated = reward > 1 - 1e-6
         truncated = (self._t >= self._max_steps)
 
-        if self.render_mode == "human":
-            self._render_frame()
-
         return obs, reward, terminated, truncated, info
 
     def render(self):
         if self.render_mode == "rgb_array":
             return self._render_frame()
 
+    def _init_x_mark(self):
+        self._x_corners = [
+            np.random.randint(2, 9),
+            np.random.randint(2, 9),
+            np.random.randint(24, 31),
+            np.random.randint(24, 31),
+            np.random.randint(24, 31),
+            np.random.randint(2, 9),
+            np.random.randint(2, 9),
+            np.random.randint(24, 31)
+        ]
+
+    def _init_agent_mark(self):
+        # random points for drawing the cross
+        self._a_corners = [
+            np.random.randint(13, 20),
+            np.random.randint(2, 9),
+            np.random.randint(2, 9),
+            np.random.randint(24, 31),
+            np.random.randint(24, 31),
+            np.random.randint(24, 31)
+        ]
+
+    def _init_digits(self):
+        self._digit_idx = np.zeros(10, dtype=np.int64)
+        for i in self._labels:
+            self._digit_idx[i] = np.random.choice(self._labels[i])
+
     def _render_frame(self):
         canvas = pygame.Surface((self._shape[1]*32, self._shape[0]*32))
-        canvas.fill((30, 30, 30))
+        canvas.fill((0, 0, 0))
 
         for i in range(self._shape[0]):
             for j in range(self._shape[1]):
                 bg, tile = self._map[i][j]
                 if bg == "0":
-                    digit = self._data[self._digit_idx[0]]
+                    if self.rand_digits:
+                        digit_idx = np.random.choice(self._labels[0])
+                    else:
+                        digit_idx = self._digit_idx[0]
+                    digit = self._data[digit_idx]
                     digit = np.stack([digit]*3, axis=-1)
                     digit = pygame.surfarray.make_surface(np.transpose(digit, (1, 0, 2)))
                     bg_tile = pygame.transform.scale(digit, (32, 32))
                 else:
                     bg_tile = pygame.Surface((32, 32))
-                    bg_tile.fill((30, 30, 30))
+                    bg_tile.fill((0, 0, 0))
                 canvas.blit(bg_tile, (j*32, i*32))
 
                 if tile == "#":
@@ -140,29 +179,68 @@ class MNISTSokoban(gym.Env):
                     rect = pygame.Rect(j*32, i*32, 32, 32)
                     pygame.draw.rect(canvas, color, rect)
                 elif tile == "@":
-                    # load the agent from data/wizard.png
-                    agent = pygame.image.load("data/wizard.png")
-                    agent = pygame.transform.scale(agent, (32, 32))
-                    canvas.blit(agent, (j*32, i*32))
+                    if self.rand_agent:
+                        self._init_agent_mark()
+                    color = (255, 255, 255)
+                    width = 4
+                    pygame.draw.line(canvas, color,
+                                     (j*32+self._a_corners[0], i*32+self._a_corners[1]),
+                                     (j*32+self._a_corners[2], i*32+self._a_corners[3]),
+                                     width)
+                    pygame.draw.line(canvas, color,
+                                     (j*32+self._a_corners[0], i*32+self._a_corners[1]),
+                                     (j*32+self._a_corners[4], i*32+self._a_corners[5]),
+                                     width)
+                    pygame.draw.line(canvas, color,
+                                     (j*32+self._a_corners[2], i*32+self._a_corners[3]),
+                                     (j*32+self._a_corners[4], i*32+self._a_corners[5]),
+                                     width)
+                    pygame.draw.circle(canvas, color,
+                                       (j*32+self._a_corners[0], i*32+self._a_corners[1]),
+                                       width//2)
+                    pygame.draw.circle(canvas, color,
+                                       (j*32+self._a_corners[2], i*32+self._a_corners[3]),
+                                       width//2)
+                    pygame.draw.circle(canvas, color,
+                                       (j*32+self._a_corners[4], i*32+self._a_corners[5]),
+                                       width//2)
                 elif tile != " ":
-                    digit = self._digit_idx[int(self._map[i][j][1])]
-                    digit = self._data[digit]
+                    digit = int(self._map[i][j][1])
+                    if self.rand_digits:
+                        digit_idx = np.random.choice(self._labels[digit])
+                    else:
+                        digit_idx = self._digit_idx[digit]
+                    digit = self._data[digit_idx]
                     digit = np.stack([digit]*3, axis=-1)
                     tile = pygame.surfarray.make_surface(np.transpose(digit, (1, 0, 2)))
                     # scale the tile to 32x32
                     tile = pygame.transform.scale(tile, (32, 32))
                     canvas.blit(tile, (j*32, i*32))
                     if bg == "0":
+                        if self.rand_x:
+                            self._init_x_mark()
                         color = (255, 255, 255)
                         width = 4
-                        pygame.draw.line(canvas, color, (j*32+self._ax, i*32+self._ay),
-                                         (j*32+self._bx, i*32+self._by), width)
-                        pygame.draw.line(canvas, color, (j*32+self._cx, i*32+self._cy),
-                                         (j*32+self._dx, i*32+self._dy), width)
-                        pygame.draw.circle(canvas, color, (j*32+self._ax, i*32+self._ay), width//2)
-                        pygame.draw.circle(canvas, color, (j*32+self._bx, i*32+self._by), width//2)
-                        pygame.draw.circle(canvas, color, (j*32+self._cx, i*32+self._cy), width//2)
-                        pygame.draw.circle(canvas, color, (j*32+self._dx, i*32+self._dy), width//2)
+                        pygame.draw.line(canvas, color,
+                                         (j*32+self._x_corners[0], i*32+self._x_corners[1]),
+                                         (j*32+self._x_corners[2], i*32+self._x_corners[3]),
+                                         width)
+                        pygame.draw.line(canvas, color,
+                                         (j*32+self._x_corners[4], i*32+self._x_corners[5]),
+                                         (j*32+self._x_corners[6], i*32+self._x_corners[7]),
+                                         width)
+                        pygame.draw.circle(canvas, color,
+                                           (j*32+self._x_corners[0], i*32+self._x_corners[1]),
+                                           width//2)
+                        pygame.draw.circle(canvas, color,
+                                           (j*32+self._x_corners[2], i*32+self._x_corners[3]),
+                                           width//2)
+                        pygame.draw.circle(canvas, color,
+                                           (j*32+self._x_corners[4], i*32+self._x_corners[5]),
+                                           width//2)
+                        pygame.draw.circle(canvas, color,
+                                           (j*32+self._x_corners[6], i*32+self._x_corners[7]),
+                                           width//2)
 
         if self.render_mode == "human":
             if self._window is None:
@@ -177,11 +255,10 @@ class MNISTSokoban(gym.Env):
             pygame.event.pump()
             pygame.display.update()
             self._clock.tick(self.metadata["render_fps"])
-        else:
-            return np.transpose(pygame.surfarray.array3d(canvas), (1, 0, 2))
+        return np.transpose(pygame.surfarray.array3d(canvas)[:, :, 0], (1, 0))
 
     def _get_obs(self) -> np.ndarray:
-        return self._map
+        return self._render_frame()
 
     def _get_info(self) -> dict:
         return {"map": self._map}
@@ -197,6 +274,15 @@ class MNISTSokoban(gym.Env):
                     if (fg != "#" and fg != " " and fg != "@"):
                         n_crossed += 1
         return n_crossed / n_total
+
+    def _match_indices(self, state, next_state):
+        # TODO
+        # normally, we need to figure out which entity maps to which one
+        # e.g., maybe with something like the Hungarian algorithm
+        feat, _ = state[:, :self._feat_dim], state[:, self._feat_dim:]
+        feat_n, _ = next_state[:, :self._feat_dim], next_state[:, self._feat_dim:]
+        indices = np.argmin(cdist(feat, feat_n), axis=-1)
+        return indices
 
     @property
     def map(self) -> np.ndarray:
@@ -221,25 +307,21 @@ class MNISTSokoban(gym.Env):
     def generate_map(size: tuple[int, int] = (10, 10), max_crates: int = 5) -> list[list[str]]:
         ni, nj = size
         assert ni >= 3 and nj >= 3, "The size of the map must be at least 3x3"
-        total_middle_tiles = (ni-4)*(nj-4)
+        total_middle_tiles = (ni-2)*(nj-2)
         assert (2*max_crates+1) <= total_middle_tiles, \
             "The number of crates (together with their goals) must be less than the total non-edge empty tiles"
 
         _map = [[(" ", " ") for _ in range(nj)] for _ in range(ni)]
-        for i in range(ni):
-            for j in range(nj):
-                if i == 0 or i == ni-1 or j == 0 or j == nj-1:
-                    _map[i][j] = (" ", "#")
 
         n = np.random.randint(1, max_crates+1)
         digits = np.random.randint(1, 10, n)
-        locations = np.random.permutation((ni-4)*(nj-4))[:(2*n+1)]
+        locations = np.random.permutation((ni-2)*(nj-2))[:(2*n+1)]
         for i, x_i in enumerate(digits):
-            di, dj = locations[i] // (nj-4) + 2, locations[i] % (nj-4) + 2
+            di, dj = locations[i] // (nj-2) + 1, locations[i] % (nj-2) + 1
             _map[di][dj] = (" ", str(x_i))
-            di, dj = locations[i+n] // (nj-4) + 2, locations[i+n] % (nj-4) + 2
+            di, dj = locations[i+n] // (nj-2) + 1, locations[i+n] % (nj-2) + 1
             _map[di][dj] = ("0", " ")
-        ax, ay = locations[-1] // (nj-4) + 2, locations[-1] % (nj-4) + 2
+        ax, ay = locations[-1] // (nj-2) + 1, locations[-1] % (nj-2) + 1
         _map[ax][ay] = (" ", "@")
         return _map
 
