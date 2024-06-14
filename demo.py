62f3e9af40d2bbb6de74de685c36068df77898e2
alper111 <ahmetoglu.alper@gmail.com>
2024-06-14 16:48:09 -0400

    update demo script w.r.t. all changes

--- demo.py
+++ demo.py
@@ -1,9 +1,14 @@
+import logging
 import pickle
+import argparse
+
+from gymnasium.wrappers.flatten_observation import FlattenObservation
+from gymnasium.wrappers.transform_observation import TransformObservation
 
 from environment import MNIST8Tile, ObjectCentricEnv
 from collect import collect
 from partition import partition_to_subgoal
-from learn_ops import learn_operators
+from factorise import factors_from_partitions, add_factors_to_partitions
 from build_vocab import build_vocabulary, build_schemata
 
 
@@ -15,44 +20,57 @@ logger = logging.getLogger("main")
 
 def main(n_samples: int, object_factored: bool):
     # initialize the environment
-    env = ObjectCentricEnv(MNIST8Tile(random=False, max_steps=1))
+    if object_factored:
+        env = ObjectCentricEnv(MNIST8Tile(random=True, max_steps=1))
+    else:
+        env = FlattenObservation(MNIST8Tile(random=False, max_steps=1))
+        env = TransformObservation(env, lambda x: x / 255)
 
     # collect data
-    print("Collecting data...")
-    dataset = collect(10000, env, None)
-    print("Data collected.")
+    logger.info("Collecting data...")
+    dataset = collect(n_samples, env, None)
+    logger.info("Data collected.")
 
     # partition the data s.t. subgoal property is satisfied
-    print("Partitioning data...")
+    logger.info("Partitioning data...")
     partitions = partition_to_subgoal(dataset)
-    print(f"Number of partitions={len(partitions)}.")
+    logger.info(f"Number of partitions={len(partitions)}.")
     with open("partitions.pkl", "wb") as f:
         pickle.dump(partitions, f)
 
-    # learn precondition classifiers and effect predictors
-    print("Learning operators...")
-    ops = learn_operators(partitions)
-    with open("ops.pkl", "wb") as f:
-        pickle.dump(ops, f)
-    print(f"Number of operators={len(ops)}.")
-    # ops = pickle.load(open("ops.pkl", "rb"))
+    # learn factors
+    logger.info("Learning factors...")
+    factors = factors_from_partitions(partitions, threshold=0.9)
+    add_factors_to_partitions(partitions, factors, threshold=0.9)
 
-    print("Building vocabulary...")
-    vocabulary, propositions = build_vocabulary(ops, 786)
+    with open("factors.pkl", "wb") as f:
+        pickle.dump(factors, f)
+    logger.info(f"Number of factors={len(factors)}.")
+
+    logger.info("Building vocabulary...")
+    vocabulary, pre_props, eff_props = build_vocabulary(partitions, factors)
     with open("vocabulary.pkl", "wb") as f:
         pickle.dump(vocabulary, f)
-    with open("propositions.pkl", "wb") as f:
-        pickle.dump(propositions, f)
-    print(f"Vocabulary size={len(vocabulary)}.")
-    print(f"Number of propositions={len(propositions)}.")
-    # factors = pickle.load(open("factors.pkl", "rb"))
-    # vocabulary = pickle.load(open("vocabulary.pkl", "rb"))
-    # propositions = pickle.load(open("propositions.pkl", "rb"))
+    with open("pre_props.pkl", "wb") as f:
+        pickle.dump(pre_props, f)
+    with open("eff_props.pkl", "wb") as f:
+        pickle.dump(eff_props, f)
+    logger.info(f"Vocabulary size={len(vocabulary)}.")
+    logger.info(f"Number of precondition propositions={len(pre_props)}.")
+    logger.info(f"Number of effect propositions={len(eff_props)}.")
 
-    print("Building schemata...")
-    schemata = build_schemata(ops, vocabulary, propositions)
+    logger.info("Building schemata...")
+    schemata = build_schemata(vocabulary, pre_props, eff_props)
     with open("schemata.pkl", "wb") as f:
         pickle.dump(schemata, f)
     for schema in schemata:
-        print(schema)
-    print(f"Number of action schemas={len(schemata)}.")
+        logger.info(schema)
+    logger.info(f"Number of action schemas={len(schemata)}.")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--n_samples", type=int, default=5000)
+    parser.add_argument("--object_factored", action="store_true")
+    args = parser.parse_args()
+    main(args.n_samples, args.object_factored)
