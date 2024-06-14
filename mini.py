b5e895abff5bb49b394689c8b0360b922c31a9c6
alper111 <ahmetoglu.alper@gmail.com>
2024-06-14 16:47:50 -0400

    update sample script for mnistsokoban

--- mini.py
+++ mini.py
@@ -2,7 +2,9 @@ from environment import MNISTSokoban
 
 
 if __name__ == "__main__":
-    env = MNISTSokoban(size=(7, 7), max_crates=2, max_steps=200, render_mode="human")
+    # env = MNISTSokoban(map_file="map1.txt", max_crates=2, max_steps=200, render_mode="human")
+    env = MNISTSokoban(size=(5, 5), max_crates=3, max_steps=50, render_mode="human",
+                       rand_digits=True, rand_agent=True, rand_x=True)
     for _ in range(2000):
         env.reset()
         done = False
