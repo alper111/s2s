b35852fba2e09fbeb0c889c77a4b4c869069a188
alper111 <ahmetoglu.alper@gmail.com>
2024-06-24 11:28:33 -0400

    rename sokoban demo file

--- demo_sokoban.py
+++ demo_sokoban.py
@@ -1,12 +1,13 @@
-from environment import MNISTSokoban
+from environments.sokoban import MNISTSokoban
 
 
 if __name__ == "__main__":
     # env = MNISTSokoban(map_file="map1.txt", max_crates=2, max_steps=200, render_mode="human")
-    env = MNISTSokoban(size=(5, 5), max_crates=3, max_steps=50, render_mode="human",
-                       rand_digits=True, rand_agent=True, rand_x=True)
+    all_random = False
+    env = MNISTSokoban(size=(5, 5), max_crates=2, max_steps=50, object_centric=False,
+                       render_mode="human", rand_digits=all_random, rand_agent=all_random, rand_x=all_random)
     for _ in range(2000):
-        env.reset()
+        obs, _ = env.reset()
         done = False
         while not done:
             action = env.action_space.sample()
