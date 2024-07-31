719e333c4263749afff117575bdf55bc6fbcaa35
alper111 <ahmetoglu.alper@gmail.com>
2024-07-31 18:20:36 -0400

    initial markov state abs training script

---
+++ src/train_msa.py
@@ -0,0 +1,71 @@
+import os
+import argparse
+
+import torch
+
+from abstraction.msa import MarkovStateAbstraction
+from environments.minecraft import MinecraftDataset
+from environments.npuzzle import NPuzzleDataset
+
+
+def main(args):
+    data_path = os.path.join("data", args.env)
+    if args.env == "npuzzle":
+        dataset = NPuzzleDataset(data_path)
+    elif args.env == "sokoban":
+        raise NotImplementedError
+    elif args.env == "minecraft":
+        dataset = MinecraftDataset(data_path)
+    else:
+        raise ValueError
+
+    loader = torch.utils.data.DataLoader(dataset, batch_size=args.batch_size, shuffle=True,
+                                         collate_fn=dataset.collate_fn)
+
+    msa = MarkovStateAbstraction(input_dims=[("objects", 786)],
+                                 action_dim=4,
+                                 n_hidden=args.n_hidden,
+                                 n_latent=args.n_latent).to(args.device)
+    optimizer = torch.optim.Adam(msa.parameters(), lr=args.lr)
+
+    for e in range(args.epoch):
+        avg_inv_loss = 0
+        avg_density_loss = 0
+        avg_reg_loss = 0
+        for x, a, x_ in loader:
+            n = x["objects"].shape[0]
+            x_n, _, _ = dataset.sample(n)
+            inv_loss, density_loss, reg_loss = msa.loss(x, x_, x_n, a)
+            loss = inv_loss + density_loss + 0.01*reg_loss
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            avg_inv_loss += inv_loss.item()
+            avg_density_loss += density_loss.item()
+            avg_reg_loss += reg_loss.item()
+        print(f"Epoch {e + 1}/{args.epoch}, inv_loss={avg_inv_loss / len(loader):.5f}, "
+            f"density_loss={avg_density_loss / len(loader):.5f}, "
+            f"smoothness_loss={avg_reg_loss / len(loader):.5f}")
+
+        if (e+1) % 20 == 0:
+            save_folder = os.path.join("save", args.env)
+            os.makedirs(save_folder, exist_ok=True)
+            save_path = os.path.join(save_folder, f"{args.out}.pt")
+            torch.save(msa.cpu().state_dict(), save_path)
+            msa.to(args.device)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--env", type=str, help="The environment name.")
+    parser.add_argument("--batch-size", type=int, default=128, help="Batch size")
+    parser.add_argument("--lr", type=float, default=1e-4, help="Learning rate")
+    parser.add_argument("--epoch", type=int, default=1000, help="Number of epochs")
+    parser.add_argument("--device", type=str, default="cpu", help="Training device")
+    parser.add_argument("--n-hidden", type=int, default=256, help="Number of hidden units")
+    parser.add_argument("--n-latent", type=int, default=16, help="Number of latent units")
+    parser.add_argument("--out", type=str, help="Output name")
+    args = parser.parse_args()
+    main(args)
