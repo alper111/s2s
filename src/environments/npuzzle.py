2c6b5af98672dac2ee566c3daa6ece5aae022c49
alper111 <ahmetoglu.alper@gmail.com>
2024-08-04 16:25:45 -0400

    update npuzzle dataset for new arch

--- src/environments/npuzzle.py
+++ src/environments/npuzzle.py
@@ -255,4 +255,6 @@ class NPuzzle(gym.Env):
 class NPuzzleDataset(UnorderedDataset):
     @staticmethod
     def _actions_to_label(action, key_order):
-        return torch.tensor(action, dtype=torch.long)
+        a = torch.zeros(len(key_order)+1, dtype=torch.long)
+        a[0] = action
+        return a
