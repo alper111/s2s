77f3bf27ef7cef1248572f81fd804276383e09a6
alper111 <ahmetoglu.alper@gmail.com>
2024-09-23 17:31:15 -0400

    init from str

--- src/main.py
+++ src/main.py
@@ -1,9 +1,7 @@
 import sys
-import yaml
 
 from agent import Agent
 
-config = yaml.safe_load(open(sys.argv[1], "r"))
-agent = Agent(config)
+agent = Agent(sys.argv[1])
 agent.train_abstraction()
 agent.fit_s2s()
